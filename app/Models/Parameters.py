aa0bbcf4da1639b8af69ba4418f9a1d4b32a5c2c
Rafał Salamon <rasa@salamonrafal.pl>
2022-03-22 17:55:55 +0100

    Update: - added command params as an object representation - added builder command params - added simply DI - added several exceptions

--- app/Models/Parameters.py
+++ app/Models/Parameters.py
@@ -13,18 +13,18 @@ class Parameters(ShortAliases):
     silent: bool
 
     def __init__(
-        self,
-        command: str = "",
-        image_name: str = "",
-        docker_file: str = "",
-        version: str = "",
-        action: str = "",
-        environment: str = "",
-        container_name: str = "",
-        silent: bool = False
+            self,
+            command: str = "",
+            image_name: str = "",
+            docker_file: str = "",
+            version: str = "",
+            action: str = "",
+            environment: str = "",
+            container_name: str = "",
+            silent: bool = False
     ):
         super(Parameters, self).__init__()
-        
+
         self.__update(
             command,
             image_name,
@@ -36,17 +36,17 @@ class Parameters(ShortAliases):
             silent
         )
         pass
-    
+
     def __update(
-        self,
-        command: str,
-        image_name: str,
-        docker_file: str,
-        version: str,
-        action: str,
-        environment: str,
-        container_name: str,
-        silent: bool
+            self,
+            command: str,
+            image_name: str,
+            docker_file: str,
+            version: str,
+            action: str,
+            environment: str,
+            container_name: str,
+            silent: bool
     ):
         self.image_name = image_name
         self.docker_file = docker_file
@@ -59,38 +59,62 @@ class Parameters(ShortAliases):
         pass
 
     # image_name: i
-    def get_image_name(self) -> str: return self.image_name
-    def set_image_name(self, image_name: str): self.image_name = image_name
-    
+    def get_image_name(self) -> str:
+        return self.image_name
+
+    def set_image_name(self, image_name: str):
+        self.image_name = image_name
+
     # container_name: cn
-    def get_container_name(self) -> str: return self.container_name
-    def set_container_name(self, container_name: str): self.container_name = container_name
+    def get_container_name(self) -> str:
+        return self.container_name
+
+    def set_container_name(self, container_name: str):
+        self.container_name = container_name
 
     # docker_file: d
-    def get_docker_file(self) -> str: return self.docker_file
-    def set_docker_file(self, docker_file: str): self.docker_file = docker_file
+    def get_docker_file(self) -> str:
+        return self.docker_file
+
+    def set_docker_file(self, docker_file: str):
+        self.docker_file = docker_file
 
     # version: vd
-    def get_version(self) -> str: return self.version
-    def set_version(self, version: str): self.version = version
+    def get_version(self) -> str:
+        return self.version
+
+    def set_version(self, version: str):
+        self.version = version
 
     # command: c
     # cache build: image_name,docker_file,version,environment
-    def get_command(self) -> str: return self.command
-    def set_command(self, command: str): self.command = command
+    def get_command(self) -> str:
+        return self.command
+
+    def set_command(self, command: str):
+        self.command = command
 
     # action: a
-    def get_action(self) -> str: return self.action
-    def set_action(self, action: str): self.action = action
+    def get_action(self) -> str:
+        return self.action
+
+    def set_action(self, action: str):
+        self.action = action
 
     # environment: e
-    def get_environment(self) -> str: return self.environment
-    def set_environment(self, environment: str): self.environment = environment
-    
+    def get_environment(self) -> str:
+        return self.environment
+
+    def set_environment(self, environment: str):
+        self.environment = environment
+
     # silent: s
-    def get_silent(self) -> bool: return self.silent
-    def set_silent(self, silent: bool): self.silent = silent
-    
+    def get_silent(self) -> bool:
+        return self.silent
+
+    def set_silent(self, silent: bool):
+        self.silent = silent
+
     def __getitem__(self, item):
         if item == "image_name":
             return self.get_image_name()
@@ -105,10 +129,10 @@ class Parameters(ShortAliases):
         elif item == "environment":
             return self.get_environment()
         elif item == "silent":
-            return "Yes" if self.get_silent() == True else "No"
+            return "Yes" if self.get_silent() is True else "No"
         else:
             return None
-        
+
     def __setitem__(self, key, value):
         if key == "image_name":
             return self.set_image_name(value)
@@ -126,14 +150,14 @@ class Parameters(ShortAliases):
             return self.set_silent(value)
         else:
             return None
-        
+
     def __str__(self):
-        return "image_name: {}, docker_file: {}, version: {}, command: {}, action: {}, environment: {}"\
+        return "image_name: {}, docker_file: {}, version: {}, command: {}, action: {}, environment: {}" \
             .format(
-                self.get_image_name(),
-                self.get_docker_file(),
-                self.get_version(),
-                self.get_command(),
-                self.get_action(),
-                self.get_environment(),
-            )
+            self.get_image_name(),
+            self.get_docker_file(),
+            self.get_version(),
+            self.get_command(),
+            self.get_action(),
+            self.get_environment(),
+        )
